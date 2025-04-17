448195c7291c8b1b34455110a655d54e7e37b879
Dominic Hakke <d.hakke@yobiiq.com>
2025-04-17 16:16:41 +0200

    Directory style for datacake

---
+++ IQ-EM2101/Datacake/IQ-EM2101-datacake.js
@@ -0,0 +1,23 @@
+
+/**
+ *__   _____  ____ ___ ___ ___  
+ *\ \ / / _ \| __ )_ _|_ _/ _ \ 
+ * \ V / | | |  _ \| | | | | | |
+ * | || |_| | |_) | | | | |_| |
+ * |_| \___/|____/___|___\__\_\
+ *                              
+ * 
+ * YOBIIQ JS payload decoder compatible with Datacake.
+ * 
+ * @author      Fostin Kpodar <f.kpodar@yobiiq.com>
+ * @version     1.0.0
+ * @copyright   YOBIIQ B.V. | https://www.yobiiq.com
+ * 
+ * @release     07/16/2023
+ * @update      11/05/2024
+ * 
+ *
+ * @product     P1002009 iQ EM2101 STD (1-phase energy meter)
+ * 
+ * 
+ */
